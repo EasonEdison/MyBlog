7b8c9bc24d6b8a1fa86d5dc72bf7b21cf834879d
tangxiaohan <tangxiaohan@sensetime.com>
2020-12-21 09:56:09 +0800

    bug_1

---
+++ MyPPro/src/main/java/com/example/demo/TTTT/main.java
@@ -0,0 +1,13 @@
+package com.example.demo.TTTT;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+    }
+}
