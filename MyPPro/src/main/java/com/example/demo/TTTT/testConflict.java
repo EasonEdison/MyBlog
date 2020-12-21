4d5ba8c88218f7298ce13ea8d8a21ff245508c08
tangxiaohan <tangxiaohan@sensetime.com>
2020-12-21 10:11:00 +0800

    bug_5

--- MyPPro/src/main/java/com/example/demo/TTTT/testConflict.java
+++ MyPPro/src/main/java/com/example/demo/TTTT/testConflict.java
@@ -3,5 +3,9 @@ package com.example.demo.TTTT;
 public class testConflict {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
     }
 }
